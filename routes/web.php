3fd675278d99321c9f263cf4a3681c8383721b0f
rakhargo <dhifiargo2004@gmail.com>
2023-06-12 17:22:24 +0700

    Permulaan

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/tabVoucher', function () {
+    return view('tabVoucher');
+});
+
+Route::get('/tabDetails', function () {
+    return view('tabDetails');
+});
+
+Route::get('/tabContact', function () {
+    return view('tabContact');
+});
