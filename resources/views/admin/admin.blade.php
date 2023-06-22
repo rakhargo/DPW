80df3f5ab742c28df4a28fedb9e288757bb22db9
rakhargo <dhifiargo2004@gmail.com>
2023-06-22 10:56:32 +0700

    ubah style

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -21,28 +21,10 @@
     <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
 <!--
 
-TemplateMo 589 lugx gaming
-
-https://templatemo.com/tm-589-lugx-gaming
-
 -->
   </head>
 
 <body>
-
-  <!-- ***** Preloader Start ***** -->
-  {{-- <div id="js-preloader" class="js-preloader">
-    <div class="preloader-inner">
-      <span class="dot"></span>
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-  </div> --}}
-  <!-- ***** Preloader End ***** -->
-
   <!-- ***** Header Area Start ***** -->
   <header class="header-area header-sticky">
     <div class="container">
