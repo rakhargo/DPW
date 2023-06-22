81b90166ce71f67773922bd30635663d8525e3c6
Shisones <shisones745@gmail.com>
2023-06-22 10:57:36 +0700

    style ce es es

--- resources/views/tabInvoice.blade.php
+++ resources/views/tabInvoice.blade.php
@@ -41,7 +41,7 @@
 
     <!-- Additional CSS Files -->
     <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-lugx-gaming.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/owl.css">
     <link rel="stylesheet" href="assets/css/animate.css">
     <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
